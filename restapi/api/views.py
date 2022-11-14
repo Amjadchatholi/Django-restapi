6d415a8fe4515562f735cffcf1d4fd109e0cef0d
Amjad Chatholi <amjadchatholi@gmail.com>
2022-11-14 11:19:44 -0800

    token authentication api

--- restapi/api/views.py
+++ restapi/api/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from .serializers import UserRegister 
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
 class register(APIView):
@@ -20,3 +21,10 @@ class register(APIView):
         else:
             data=serializer.errors
         return Response(data)
+
+class welcome(APIView):
+    permission_classes =(IsAuthenticated,)
+    
+    def get(self,request):
+        content={'user':str(request.user),'userid':str(request.user.id)}
+        return Response(content)
